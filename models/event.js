76163744456a5520d557124ee27d8883deedf255
nsutter <suttercyril.sutternicolas@gmail.com>
2016-12-01 20:26:47 +0100

    fzeef

---
+++ models/event.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var Event = new Schema({
+  title: String,
+  what: String,
+  when: Date,
+  longitude: Number,
+  latitude: Number,
+  who: String
+});
+
+Event.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('Event', Event);
